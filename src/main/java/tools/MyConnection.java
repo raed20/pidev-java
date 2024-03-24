9cdd9db87a4e4bb28a0d80dd29b42a93f6497aba
aminbrahmi <aminebrahmity12@gmail.com>
2024-03-24 08:17:06 +0100

    Category CRUD

--- src/main/java/tools/MyConnection.java
+++ src/main/java/tools/MyConnection.java
@@ -1,4 +1,36 @@
 package tools;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MyConnection {
+    //DB
+    final String URL="jdbc:mysql://localhost:3306/pidev";
+    final String USERNAME ="root";
+    final String PASSWORD ="";
+
+    //Att
+    private Connection connection;
+    private static MyConnection instance;
+    //Constructor
+    //Singleton step1
+    private MyConnection(){
+        try {
+            connection= DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            System.out.println("Connection Successful !");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static MyConnection getInstance() {
+        if(instance== null)
+            instance=new MyConnection();
+        return instance;
+    }
 }
