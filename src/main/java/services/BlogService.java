0b1f0fb8dba1e968afe62f4aecbe7ae60464a612
AbdouB03 <abdoubouafif10@gmail.com>
2024-04-20 03:19:33 +0100

    CrudBlogFront

--- src/main/java/services/BlogService.java
+++ src/main/java/services/BlogService.java
@@ -6,8 +6,7 @@ import entities.Blog;
 import entities.Commentaire;
 import interfaces.IBlog;
 import tools.MyConnection;
-import java.sql.Statement;
-import java.sql.Connection;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
