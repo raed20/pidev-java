8b8b917800da8ca5f1ed4cc764533f0eb24458c5
Kaysbt <159252061+Kaysbt@users.noreply.github.com>
2024-04-23 15:12:28 +0200

    Update MainUser.java

--- src/main/java/org/example/MainUser.java
+++ src/main/java/org/example/MainUser.java
@@ -21,28 +21,4 @@ public class MainUser extends Application {
         launch(args);
     }
 }
-/*
-package org.example;
 
-        import javafx.application.Application;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Scene;
-        import javafx.stage.Stage;
-
-        import java.io.IOException;
-
-public class MainUser extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/tn/esprit/application/FRONT/Home.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(MainUser.class.getResource("/FrontOffice/Login/login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
-}*/
