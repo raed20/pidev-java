025d76225202b9d5ec3aa4504382aeff480a6f92
raed20 <raedmaaloul3@gmail.com>
2024-05-03 19:20:53 +0100

    add rss , generate code , dashbord back

--- src/main/java/tests/MainFX.java
+++ src/main/java/tests/MainFX.java
@@ -33,51 +33,6 @@ public class MainFX extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(true);
         primaryStage.show();
-        /*WebView webView = new WebView();
-        webView.getEngine().load("https://app.powerbi.com/groups/900809af-865b-4244-a487-4110c5e5926c/reports/d4ef8903-50e7-4839-9501-0ca2e4a9bad0/ReportSection?experience=power-bi");
-
-        StackPane root = new StackPane();
-        root.getChildren().add(webView);
-
-        Scene scene = new Scene(root, 800, 600);
-
-        primaryStage.setTitle("Web View Example");
-        primaryStage.setScene(scene);
-        primaryStage.show();*/
-        // Définissez les paramètres de l'authentification
-      /*
-
-
-        ExecutorService service = Executors.newFixedThreadPool(1);
-        try {
-            // Authenticate and obtain an embed token
-            AuthenticationContext context = new AuthenticationContext(AUTHORITY, false, service);
-            ClientCredential credential = new ClientCredential(CLIENT_ID, CLIENT_SECRET);
-            Future<AuthenticationResult> future = context.acquireToken("https://analysis.windows.net/powerbi/api", credential, null);
-            AuthenticationResult result = future.get();
-            String embedToken = result.getAccessToken();
-            System.out.println(embedToken);
-
-            // Load the Power BI dashboard in the WebView using the embed token
-            WebView webView = new WebView();
-            WebEngine webEngine = webView.getEngine();
-
-            String embedUrl = "https://app.powerbi.com/reportEmbed?reportId=" + REPORT_ID + "&autoAuth=true&groupId=" + GROUP_ID;
-            String htmlContent = "<html><body><iframe src=\"" + embedUrl + "&token=" + embedToken + "\" width=\"1140\" height=\"541.25\" frameborder=\"0\" allowFullScreen=\"true\"></iframe></body></html>";
-            webEngine.loadContent(htmlContent);
-
-            // Create the scene and show it
-            StackPane root = new StackPane();
-            root.getChildren().add(webView);
-            Scene scene = new Scene(root, 800, 600);
-            primaryStage.setTitle("Power BI Embedded");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            service.shutdown();
-        }*/
     }
 
         public static void main(String[] args) {
